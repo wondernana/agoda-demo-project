89b5ae287b60a9996e2fb1f218318895018496f7
Alena <86495466+wondernana@users.noreply.github.com>
2022-06-05 23:01:48 +0300

    update video attachment

--- src/test/java/cloud/autotests/helpers/AllureAttachments.java
+++ src/test/java/cloud/autotests/helpers/AllureAttachments.java
@@ -1,16 +1,10 @@
 package cloud.autotests.helpers;
 
-import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class AllureAttachments {
     public static final Logger LOGGER = LoggerFactory.getLogger(AllureAttachments.class);
@@ -34,44 +28,9 @@ public class AllureAttachments {
         return DriverUtils.getPageSourceAsBytes();
     }
 
-    @Attachment(value = "VideoFile", type = "video/mp4", fileExtension = ".mp4")
+    @Attachment(value = "Video file", type = "video/mp4", fileExtension = ".mp4")
     public static byte[] addVideo(String sessionId) {
-        URL videoUrl = DriverUtils.getVideoUrl(sessionId);
-        return getFileAsBytes(videoUrl);
+        URL videoUrl = URLUtils.getVideoUrl(sessionId);
+        return URLUtils.fetchBytesWithRetry(videoUrl, 3);
     }
-
-    private static byte[] getFileAsBytes(URL fileUrl) {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        URLConnection connection = null;
-        try {
-            connection = fileUrl.openConnection();
-            connection.connect();
-        } catch (IOException e) {
-            LOGGER.warn("[ALLURE VIDEO NOT FOUND] Cant establish url connection, {}", fileUrl);
-            e.printStackTrace();
-            return null;
-        }
-        if (connection != null) {
-            for (int i = 0; i < 3; i++) {
-                try (InputStream stream = connection.getInputStream()) {
-                    byte[] chunk = new byte[4096];
-                    int bytesRead;
-
-                    while ((bytesRead = stream.read(chunk)) > 0) {
-                        outputStream.write(chunk, 0, bytesRead);
-                    }
-                    break;
-                } catch (FileNotFoundException e) {
-                    // waiting for video file to be processed
-                    LOGGER.warn("[ALLURE VIDEO NOT FOUND] Cant find allure video, {}", fileUrl);
-                    e.printStackTrace();
-                    Selenide.sleep(1000);
-                } catch (IOException e) {
-                    LOGGER.warn("[ALLURE VIDEO ATTACHMENT ERROR] Cant attach allure video, {}", fileUrl);
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-        } return outputStream.toByteArray();
-    } 
 }
