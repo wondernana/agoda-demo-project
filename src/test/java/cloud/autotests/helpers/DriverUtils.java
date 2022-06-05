cfed7b0065a72fc147ac37eaf628a24d1ae6dbf9
Alena <86495466+wondernana@users.noreply.github.com>
2022-06-05 20:53:45 +0300

    update attachments

--- src/test/java/cloud/autotests/helpers/DriverUtils.java
+++ src/test/java/cloud/autotests/helpers/DriverUtils.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
@@ -31,16 +29,8 @@ public class DriverUtils {
         return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
     }
 
-    public static URL getVideoUrl(String sessionId) {
-        String videoUrl = Project.config.videoStorage() + sessionId + ".mp4";
-
-        try {
-            return new URL(videoUrl);
-        } catch (MalformedURLException e) {
-            LOGGER.warn("[ALLURE VIDEO ATTACHMENT ERROR] Wrong test video url, {}", videoUrl);
-            e.printStackTrace();
-        }
-        return null;
+    public static String getVideoUrl(String sessionId) {
+        return Project.config.videoStorage() + sessionId + ".mp4";
     }
 
     public static String getConsoleLogs() { // todo refactor
